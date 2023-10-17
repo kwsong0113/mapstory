975f452cdc566f19b7d5cc5b1330390ab925a950
Kiwhan Song <kwsong0113@gmail.com>
2023-10-17 18:35:38 -0400

    style: add shadow to MyMarker

--- client/components/Map/MyMarker.vue
+++ client/components/Map/MyMarker.vue
@@ -7,8 +7,8 @@ const { currentLocation } = storeToRefs(useLocationStore());
 </script>
 <template>
   <CustomMarker :options="{ position: currentLocation }">
-    <div class="flex justify-center items-center bg-base-100 w-[22px] h-[22px] rounded-lg">
-      <div class="bg-info w-4 h-4 rounded-lg"></div>
+    <div class="flex justify-center items-center bg-base-100 w-6 h-6 rounded-xl shadow-[0px_4px_6px_#999999]">
+      <div class="bg-info w-4 h-4 rounded-xl"></div>
     </div>
   </CustomMarker>
 </template>
